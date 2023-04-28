d1afe8af25c269d055e86b81205a5718b8f1da87
Thijs Kempeneers <r0796833@pc-klas4-8.esat.kuleuven.be>
2023-04-28 12:58:40 +0200

    pipeline bug

--- CA_Exercises/Verilog/RTL/cpu.v
+++ CA_Exercises/Verilog/RTL/cpu.v
@@ -50,10 +50,19 @@ wire [      63:0] regfile_wdata,mem_data,alu_out,
                   regfile_rdata_1,regfile_rdata_2,
                   alu_operand_2;
 
-wire signed [63:0] immediate_extended;
+wire [63:0] updated_pc_ID_EX,updated_pc_IF_ID;
+wire [31:0] instruction_IF_ID,instruction_ID_EX,instruction_EX_MEM,instruction_MEM_WB;
+wire [63:0] regfile_rdata_1_ID_EX, regfile_rdata_2_ID_EX,branch_pc_EX_MEM,jump_pc_EX_MEM,
+            alu_out_EX_MEM, regfile_rdata_2_EX_MEM,mem_data_MEM_WB,alu_out_MEM_WB;
+wire        reg_write_ID_EX,branch_ID_EX,alu_src_ID_EX,
+            mem_write_ID_EX,mem_read_ID_EX,mem_write_EX_MEM,mem_read_EX_MEM,
+            branch_EX_MEM,jump_EX_MEM,reg_write_EX_MEM,mem_2_reg_EX_MEM,mem_2_reg_MEM_WB,reg_write_MEM_WB;
+wire [1:0]  alu_op_ID_EX;
+
+wire signed [63:0] immediate_extended,immediate_extended_ID_EX;
 
 immediate_extend_unit immediate_extend_u(
-    .instruction         (instruction),
+    .instruction         (instruction_IF_ID),
     .immediate_extended  (immediate_extended)
 );
 
@@ -62,11 +71,11 @@ pc #(
 ) program_counter (
    .clk       (clk       ),
    .arst_n    (arst_n    ),
-   .branch_pc (branch_pc ),
+   .branch_pc (branch_pc_EX_MEM ),
    .jump_pc   (jump_pc   ),
-   .zero_flag (zero_flag ),
-   .branch    (branch    ),
-   .jump      (jump      ),
+   .zero_flag (zero_flag_EX_MEM),
+   .branch    (branch_EX_MEM),
+   .jump      (jump_EX_MEM),
    .current_pc(current_pc),
    .enable    (enable    ),
    .updated_pc(updated_pc)
@@ -88,14 +97,345 @@ sram_BW32 #(
    .rdata_ext(rdata_ext     )
 );
 
+
+// IF_ID Pipeline register for instruction signal
+reg_arstn_en#(
+   .DATA_W(32) // width of the forwarded signal
+)signal_pipe_IF_ID1(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (instruction   ),
+   .en         (enable        ),
+   .dout       (instruction_IF_ID)
+);
+
+// IF_ID Pipeline register for PC
+reg_arstn_en#(
+   .DATA_W(64) // width of the forwarded signal
+)signal_pipe_IF_ID2(
+   .clk        (clk              ),
+   .arst_n     (arst_n           ),
+   .din        (updated_pc       ),
+   .en         (enable           ),
+   .dout       (updated_pc_IF_ID )
+);
+
+// ID/EX Pipeline register for PC
+reg_arstn_en#(
+   .DATA_W(64) // width of the forwarded signal
+)signal_pipe_ID_EX1(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (updated_pc_IF_ID ),
+   .en         (enable           ),
+   .dout       (updated_pc_ID_EX )
+);
+
+// Pipeline register for rdata1
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_ID_EX2(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (regfile_rdata_1   ),
+   .en         (enable        ),
+   .dout       (regfile_rdata_1_ID_EX)
+);
+
+// Pipeline register for rdata2
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_ID_EX3(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (regfile_rdata_2   ),
+   .en         (enable        ),
+   .dout       (regfile_rdata_2_ID_EX)
+);
+
+// Pipeline register for imm ext
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_ID_EX4(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (immediate_extended   ),
+   .en         (enable        ),
+   .dout       (immediate_extended_ID_EX)
+);
+
+// Pipeline register for instruction
+reg_arstn_en#(
+   .DATA_W(32)
+)signal_pipe_ID_EX5(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (instruction_IF_ID),
+   .en         (enable        ),
+   .dout       (instruction_ID_EX)
+);
+
+// Pipeline register for reg_write
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_ID_EX6(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (reg_write),
+   .en         (enable        ),
+   .dout       (reg_write_ID_EX)
+);
+
+// Pipeline register for mem_2_reg
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_ID_EX10(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_2_reg),
+   .en         (enable        ),
+   .dout       (mem_2_reg_ID_EX)
+);
+
+// Pipeline register for branch
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_ID_EX7(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (branch),
+   .en         (enable),
+   .dout       (branch_ID_EX)
+);
+
+// Pipeline register for mem_read
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_ID_EX11(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_read),
+   .en         (enable),
+   .dout       (mem_read_ID_EX)
+);
+
+// Pipeline register for mem_write
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_ID_EX12(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_write),
+   .en         (enable),
+   .dout       (mem_write_ID_EX)
+);
+
+// Pipeline register for alu_op
+reg_arstn_en#(
+   .DATA_W(2)
+)signal_pipe_ID_EX8(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (alu_op),
+   .en         (enable        ),
+   .dout       (alu_op_ID_EX)
+);
+
+// Pipeline register for alu_src
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_ID_EX9(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (alu_src),
+   .en         (enable        ),
+   .dout       (alu_src_ID_EX)
+);
+
+// EX_MEM Pipeline register for instruction signal
+reg_arstn_en#(
+   .DATA_W(32)
+)signal_pipe_EX_MEM1(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (instruction_ID_EX),
+   .en         (enable        ),
+   .dout       (instruction_EX_MEM)
+);
+
+// MEM_WB Pipeline register for instruction signal
+reg_arstn_en#(
+   .DATA_W(32)
+)signal_pipe_MEM_WB1(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (instruction_EX_MEM),
+   .en         (enable        ),
+   .dout       (instruction_MEM_WB)
+);
+
+// EX_MEM Pipeline register for rdata signal
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_EX_MEM2(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (regfile_rdata_2_ID_EX),
+   .en         (enable        ),
+   .dout       (regfile_rdata_2_EX_MEM)
+);
+
+// EX_MEM Pipeline register for alu_out signal
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_EX_MEM3(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (alu_out),
+   .en         (enable        ),
+   .dout       (alu_out_EX_MEM)
+);
+
+// EX_MEM Pipeline register for zero_flag
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_EX_MEM4(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (zero_flag),
+   .en         (enable        ),
+   .dout       (zero_flag_EX_MEM)
+);
+
+// EX_MEM Pipeline register for branch_pc
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_EX_MEM5(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (branch_pc),
+   .en         (enable        ),
+   .dout       (branch_pc_EX_MEM)
+);
+
+// EX_MEM Pipeline register for jump_pc
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_EX_MEM6(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (jump_pc),
+   .en         (enable        ),
+   .dout       (jump_pc_EX_MEM)
+);
+
+// EX_MEM Pipeline register for branch
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_EX_MEM7(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (branch_ID_EX),
+   .en         (enable        ),
+   .dout       (branch_EX_MEM)
+);
+
+// Pipeline register for mem_read
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_EX_MEM8(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_read_ID_EX),
+   .en         (enable),
+   .dout       (mem_read_EX_MEM)
+);
+
+// Pipeline register for mem_write
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_EX_MEM9(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_write_ID_EX),
+   .en         (enable),
+   .dout       (mem_write_EX_MEM)
+);
+
+// Pipeline register for mem_2_reg
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_EX_MEM10(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_2_reg_ID_EX),
+   .en         (enable),
+   .dout       (mem_2_reg_EX_MEM)
+);
+
+// Pipeline register for mem_2_reg
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_EX_MEM11(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (reg_write_ID_EX),
+   .en         (enable),
+   .dout       (reg_write_EX_MEM)
+);
+
+// Pipeline register for mem_data
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_MEM_WB2(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_data),
+   .en         (enable),
+   .dout       (mem_data_MEM_WB)
+);
+
+// Pipeline register for alu_out
+reg_arstn_en#(
+   .DATA_W(64)
+)signal_pipe_MEM_WB3(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (alu_out_EX_MEM),
+   .en         (enable),
+   .dout       (alu_out_MEM_WB)
+);
+
+// Pipeline register for mem_2_reg
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_MEM_WB4(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (mem_2_reg_EX_MEM),
+   .en         (enable),
+   .dout       (mem_2_reg_MEM_WB)
+);
+
+// Pipeline register for reg_write
+reg_arstn_en#(
+   .DATA_W(1)
+)signal_pipe_MEM_WB5(
+   .clk        (clk  ),
+   .arst_n     (arst_n),
+   .din        (reg_write_EX_MEM),
+   .en         (enable),
+   .dout       (reg_write_MEM_WB)
+);
+
 sram_BW64 #(
    .ADDR_W(10)
 ) data_memory(
    .clk      (clk            ),
-   .addr     (alu_out        ),
-   .wen      (mem_write      ),
-   .ren      (mem_read       ),
-   .wdata    (regfile_rdata_2),
+   .addr     (alu_out_EX_MEM        ),
+   .wen      (mem_write_EX_MEM      ),
+   .ren      (mem_read_EX_MEM       ),
+   .wdata    (regfile_rdata_2_EX_MEM),
    .rdata    (mem_data       ),   
    .addr_ext (addr_ext_2     ),
    .wen_ext  (wen_ext_2      ),
@@ -105,7 +445,7 @@ sram_BW64 #(
 );
 
 control_unit control_unit(
-   .opcode   (instruction[6:0]),
+   .opcode   (instruction_IF_ID[6:0]),
    .alu_op   (alu_op          ),
    .reg_dst  (reg_dst         ),
    .branch   (branch          ),
@@ -117,40 +457,42 @@ control_unit control_unit(
    .jump     (jump            )
 );
 
+// ID STAGE
 register_file #(
    .DATA_W(64)
 ) register_file(
    .clk      (clk               ),
    .arst_n   (arst_n            ),
-   .reg_write(reg_write         ),
-   .raddr_1  (instruction[19:15]),
-   .raddr_2  (instruction[24:20]),
-   .waddr    (instruction[11:7] ),
+   .reg_write(reg_write_ID_EX         ),
+   .raddr_1  (instruction_IF_ID[19:15]),
+   .raddr_2  (instruction_IF_ID[24:20]),
+   .waddr    (instruction_MEM_WB[11:7] ),
    .wdata    (regfile_wdata     ),
    .rdata_1  (regfile_rdata_1   ),
    .rdata_2  (regfile_rdata_2   )
 );
 
 alu_control alu_ctrl(
-   .func7_5       (instruction[30]   ),
-   .func3          (instruction[14:12]),
-   .alu_op         (alu_op            ),
+   .func7_5       (instruction_ID_EX[30]   ),
+   .func7_0       (instruction_ID_EX[25]    ),
+   .func3          (instruction_ID_EX[14:12]),
+   .alu_op         (alu_op_ID_EX            ),
    .alu_control    (alu_control       )
 );
 
 mux_2 #(
    .DATA_W(64)
 ) alu_operand_mux (
-   .input_a (immediate_extended),
-   .input_b (regfile_rdata_2    ),
-   .select_a(alu_src           ),
+   .input_a (immediate_extended_ID_EX),
+   .input_b (regfile_rdata_2_ID_EX   ),
+   .select_a(alu_src_ID_EX           ),
    .mux_out (alu_operand_2     )
 );
 
 alu#(
    .DATA_W(64)
 ) alu(
-   .alu_in_0 (regfile_rdata_1 ),
+   .alu_in_0 (regfile_rdata_1_ID_EX),
    .alu_in_1 (alu_operand_2   ),
    .alu_ctrl (alu_control     ),
    .alu_out  (alu_out         ),
@@ -161,17 +503,17 @@ alu#(
 mux_2 #(
    .DATA_W(64)
 ) regfile_data_mux (
-   .input_a  (mem_data     ),
-   .input_b  (alu_out      ),
-   .select_a (mem_2_reg    ),
+   .input_a  (mem_data_MEM_WB     ),
+   .input_b  (alu_out_MEM_WB      ),
+   .select_a (mem_2_reg_MEM_WB    ),
    .mux_out  (regfile_wdata)
 );
 
 branch_unit#(
    .DATA_W(64)
 )branch_unit(
-   .updated_pc         (updated_pc        ),
-   .immediate_extended (immediate_extended),
+   .updated_pc         (updated_pc_ID_EX  ),
+   .immediate_extended (immediate_extended_ID_EX),
    .branch_pc          (branch_pc         ),
    .jump_pc            (jump_pc           )
 );
